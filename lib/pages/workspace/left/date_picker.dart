f3eadadf03d24713531106212595c562ac612599
Albert Kaiser <ak@kaiserv.de>
2022-04-25 22:27:58 +0200

    fix some namings

--- lib/pages/workspace/left/date_picker.dart
+++ lib/pages/workspace/left/date_picker.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:sparta/_themes.dart';
 import 'package:sparta/pages/_shared/extensions/build_context.dart';
 import 'package:sparta/pages/_shared/extensions/date_time.dart';
+import 'package:sparta/pages/_shared/state/value_connector.dart';
 import 'package:sparta/pages/_shared/ui/simple_grid_view.dart';
 import 'package:sparta/states/events_state.dart';
 
@@ -10,21 +11,21 @@ const _rowCount = 6;
 const _availableSlots = _rowCount * _columnCount;
 
 class DatePicker extends StatelessWidget {
-  const DatePicker(this.refDate, {Key? key}) : super(key: key);
+  const DatePicker(this.pickerDate, {Key? key}) : super(key: key);
 
-  final DateTime refDate;
+  final DateTime pickerDate;
 
   Iterable<DateTime> get _dateItems {
     final items = <DateTime>[];
     var printedSlots = 0;
 
-    final firstDayOfMonth = refDate.firstDayOfMonth.midDay;
+    final firstDayOfMonth = pickerDate.firstDayOfMonth.midDay;
     for (var i = firstDayOfMonth.weekday - 1; i > 0; i--) {
       items.add(firstDayOfMonth.subtract(Duration(days: i)));
       printedSlots++;
     }
 
-    final lastDayOfMonth = refDate.lastDayOfMonth.midDay;
+    final lastDayOfMonth = pickerDate.lastDayOfMonth.midDay;
     for (var i = 0; i < lastDayOfMonth.day; i++) {
       items.add(firstDayOfMonth.add(Duration(days: i)));
       printedSlots++;
@@ -42,19 +43,24 @@ class DatePicker extends StatelessWidget {
   Widget build(BuildContext context) {
     final now = DateTime.now();
     final dateItems = _dateItems;
-    return SimpleGridView(
-      columnCount: _columnCount,
-      rowCount: _rowCount,
-      expandRows: false,
-      cellBuilder: (context, xIndex, yIndex) {
-        final cellIndex = xIndex + yIndex * _columnCount;
-        final dateItem = dateItems.elementAt(cellIndex);
-        return _DateItem(
-          dateItem,
-          isCurrentDay: dateItem.isSameDay(now),
-          isPickedMonth: dateItem.month == refDate.month,
-        );
-      },
+    // TODO check usage of this
+    return ValueConnector<DateTime?>(
+      converter: (state) => state.eventsState.focusedDate,
+      builder: (context, focusedDate) => SimpleGridView(
+        columnCount: _columnCount,
+        rowCount: _rowCount,
+        expandRows: false,
+        cellBuilder: (context, xIndex, yIndex) {
+          final cellIndex = xIndex + yIndex * _columnCount;
+          final date = dateItems.elementAt(cellIndex);
+          return _DateItem(
+            date,
+            isCurrentDay: date.isSameDay(now),
+            isFocussedDay: date.isSameDay(focusedDate),
+            isSamePickedDateMonth: date.month == pickerDate.month,
+          );
+        },
+      ),
     );
   }
 }
@@ -63,25 +69,36 @@ class _DateItem extends StatelessWidget {
   const _DateItem(
     this.date, {
     required this.isCurrentDay,
-    required this.isPickedMonth,
+    required this.isFocussedDay,
+    required this.isSamePickedDateMonth,
     Key? key,
   }) : super(key: key);
 
   final DateTime date;
   final bool isCurrentDay;
-  final bool isPickedMonth;
+  final bool isFocussedDay;
+  final bool isSamePickedDateMonth;
 
   @override
   Widget build(BuildContext context) {
     final fontWeight = isCurrentDay ? FontWeight.bold : FontWeight.normal;
 
     return GestureDetector(
-      onTap: () => context.dispatch(
-        FetchEventsAction(EventsActionType.picker, focusedDate: date),
-      ),
+      onTap: isFocussedDay
+          ? null
+          : () => context.dispatch(
+                FetchEventsAction(
+                  EventsActionType.picker,
+                  focusedDate: date,
+                ),
+              ),
       child: DecoratedBox(
         decoration: BoxDecoration(
-          color: isPickedMonth ? null : context.td.primaryColorLight,
+          color: isFocussedDay
+              ? context.td.focusColor
+              : isSamePickedDateMonth
+                  ? null
+                  : context.td.primaryColorLight,
           border: isCurrentDay ? currentDayBorder : null,
         ),
         child: Padding(
